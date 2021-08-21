71c06e6b2c80919b0b4291cc995720ab8e54b7ab
Denis <zhuravlev_denis01@mail.ru>
2021-08-21 15:55:34 +0300

    did optimization and refactoring code

--- js/modal.js
+++ js/modal.js
@@ -27,3 +27,32 @@ close_prava.addEventListener('click', function(event) {
     modal_prava.style.display = 'none'
 })
 
+// Модульное окно для footer раздел документы - пользовательское соглашение
+let open_polzovatel = document.getElementById('open_polzovatel')
+let modal_polzovatel= document.getElementById('modal_polzovatel')
+let close_polzovatel = document.getElementById('close_polzovatel')
+
+open_polzovatel.addEventListener('click', function(event) {
+    event.preventDefault()
+    modal_polzovatel.style.display = 'block'
+})
+
+close_polzovatel.addEventListener('click', function(event) {
+    event.preventDefault()
+    modal_polzovatel.style.display = 'none'
+})
+
+// Модульное окно для footer раздел документы - Cлужба потдержки
+let open_support = document.getElementById('open_support')
+let modal_support= document.getElementById('modal_support')
+let close_support = document.getElementById('close_support')
+
+open_support.addEventListener('click', function(event) {
+    event.preventDefault()
+    modal_support.style.display = 'block'
+})
+
+close_support.addEventListener('click', function(event) {
+    event.preventDefault()
+    modal_support.style.display = 'none'
+})
