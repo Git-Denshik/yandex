dc35debe93ef014d9d9219ad0995b71885db3df0
Denis <zhuravlev_denis01@mail.ru>
2021-08-22 13:01:55 +0300

    implemented  a version of the site for a tablet

---
+++ js/burger-menu.js
@@ -0,0 +1,5 @@
+// === Подключаем бургер меню ===
+document.querySelector('.btn-burger').addEventListener('click', (event) => {
+    document.querySelector('.section-header').classList.toggle('section-header--active-nav')
+})
+// === / Подключаем бургер меню  ===
